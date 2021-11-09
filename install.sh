f57a35dbd68a603ec678f333877eede97bb4c412
leonidkstr <79971428+leonidkstr@users.noreply.github.com>
2021-11-09 12:45:05 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This install making for ur linux bundle of special programs
+
+echo 
+pwd
+cd scripts
+echo 
+pwd
+echo
+./preinstall
+echo
+./packdebinstall
+echo
+./packruninstall
+echo
+./packtarinstall
+echo
+./postinstall
