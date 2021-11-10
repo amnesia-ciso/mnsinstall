36609df2a9501e60ed7032554e8db3b9881d5f09
amnesia-ciso <94020029+amnesia-ciso@users.noreply.github.com>
2021-11-10 05:59:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This install making for ur linux bundle of special programs
+chmod 777 scripts/* && chmod -x scripts/*
 mkdir tar run
 cd scripts
 echo "Please wait while install from 5 to 10 min"
