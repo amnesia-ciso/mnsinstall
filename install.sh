74dee2170e4db28605d0fe4090c3c092705b5197
amnesia-ciso <94020029+amnesia-ciso@users.noreply.github.com>
2021-11-10 06:06:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This install making for ur linux bundle of special programs
-chmod 777 scripts/* && chmod +x scripts/* && chmod +x drivers/*
+chmod 777 scripts/* && chmod +x scripts/* && chmod 777 drivers/rtl8812au-1-5.3.4/* && chmod +x drivers/rtl8812au-1-5.3.4/*
 mkdir tar run
 cd scripts
 echo "Please wait while install from 5 to 10 min"
